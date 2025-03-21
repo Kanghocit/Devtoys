4666912cafb49dfe3e0b4d4255d3323a910a38ff
Kanghocit <ankhangit06@gmail.com>
2025-03-21 15:31:30 +0700

    feat: create loading animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,12 +14,9 @@ export default function Home() {
     return (
       <div className="flex flex-col items-center justify-center h-screen">
         <img
-          src="https://banner2.cleanpng.com/20180418/fpw/avfkzsggd.webp"
-          className="w-200 h-200 object-cover rounded-full animate-spin relative"
+          src="https://i.pinimg.com/originals/71/3a/32/713a3272124cc57ba9e9fb7f59e9ab3b.gif"
+          className="w-200 h-200 object-cover "
         />
-        <div className="text-4xl text-white shadow-lg font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          Nạp tiền đi....
-        </div>
       </div>
     );
   if (error) return <div>Error: {error.message}</div>;
